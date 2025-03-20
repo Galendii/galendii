04d0cd546cf21754ff12e6dbb9ff547471872bc6
Breno Zielinski Galendi <breno.z.galendi@gmail.com>
2025-03-20 11:07:27 -0300

    chore: Add api secret

--- lib/api.ts
+++ lib/api.ts
@@ -76,6 +76,7 @@ export const sendEmail = async (email: EmailFormData) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
       },
       body: JSON.stringify(email),
     });
